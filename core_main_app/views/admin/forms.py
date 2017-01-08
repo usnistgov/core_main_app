644ab774cff5c74623db50f6af86293de522a2ba
Xavier SCHMITT <xavier.schmitt@nist.gov>
2017-01-08 15:34:40 -0500

    add form editprofile
    Change-Id: Ief488c3644da6c592a86ab1e7921739756f2e91c

--- core_main_app/views/admin/forms.py
+++ core_main_app/views/admin/forms.py
@@ -14,3 +14,13 @@ class UploadVersionForm(forms.Form):
     Form to upload a new version
     """
     xsd_file = forms.FileField(label='Select a file', required=True)
+
+
+class EditProfileForm(forms.Form):
+    """
+    Form to edit the profile information
+    """
+    firstname = forms.CharField(label='First Name', max_length=100, required=True)
+    lastname = forms.CharField(label='Last Name', max_length=100, required=True)
+    username = forms.CharField(label='Username', max_length=100, required=True, widget=forms.HiddenInput())
+    email = forms.EmailField(label='Email Address', max_length=100, required=True)
