1ddbbed8d392402f8949924c80c0bb744641e1ad
Xavier SCHMITT <xavier.schmitt@nist.gov>
2017-01-09 16:54:08 -0500

    Api user
    Change-Id: I27de1d3a8bb1a53a97398fb6867ab0bf7c214f02

---
+++ core_main_app/components/user/api.py
@@ -0,0 +1,66 @@
+"""
+    The API contains the available function to access, create and edit a user
+"""
+from django.contrib.auth.models import User
+
+
+def get_user_by_username(username):
+    """
+        Returns a user given its username
+
+        Args:
+            username (str): Given username
+
+        Returns:
+            User object
+    """
+    return User.objects.get(username=username)
+
+
+def get_user_by_id(user_id):
+    """
+        Returns a user given its primary key
+
+        Args:
+            user_id (str): Given user id
+
+        Returns:
+            User object
+    """
+    return User.objects.get(pk=user_id)
+
+
+def create_and_save_user(username, password, first_name, last_name, email):
+    """
+        Save a user with the given parameters
+
+        Args:
+            username (str): Given user name
+            password (str): Given password
+            first_name (str): Given first name
+            last_name (str): Given last name
+            email (str): Given email
+
+        Returns:
+            User object
+    """
+    user = User.objects.create_user(username=username,
+                                    password=password,
+                                    first_name=first_name,
+                                    last_name=last_name,
+                                    email=email)
+    return upsert(user)
+
+
+def upsert(user):
+    """
+
+    Args:
+        user (User): The user to be saved
+
+    Returns:
+
+    """
+    # Has to be called separately because save from django object returns nothing
+    user.save()
+    return user
