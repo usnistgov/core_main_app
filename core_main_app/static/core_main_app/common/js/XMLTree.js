dfe656839027c97cf36ade73ec1ce0a9145e54b0
Guillaume Sousa <guillaume.sousa@nist.gov>
2017-01-13 09:45:49 -0500

    Move XMLTree.js and css to main app
    Change-Id: I3997e3024abe91fbd0f110984f3ff590fea62235

---
+++ core_main_app/static/core_main_app/common/js/XMLTree.js
@@ -0,0 +1,14 @@
+/**
+ * Shows/hides a section of the XSD tree
+ * @param event
+ */
+showhide = function(event){
+	button = event.target;
+	parent = $(event.target).parent();
+	$(parent.children('ul')).toggle("blind",500);
+	if ($(button).attr("class") == "expand"){
+		$(button).attr("class","collapse");
+	}else{
+		$(button).attr("class","expand");
+	}
+};
