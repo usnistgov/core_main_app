97a5912dbf7b9648c2b8a8f2b1f435570e616594
Guillaume Sousa <guillaume.sousa@nist.gov>
2017-03-21 16:56:00 -0400

    Add data sources parameter to context processor
    Change-Id: I8f98e9cb52be258c007e49a4162b5de53b241ab8

--- core_main_app/utils/custom_context_processors.py
+++ core_main_app/utils/custom_context_processors.py
@@ -16,4 +16,7 @@ def domain_context_processor(request):
         'CUSTOM_COMPOSE': settings.CUSTOM_COMPOSE if hasattr(settings, 'CUSTOM_COMPOSE') else '',
         'CUSTOM_URL': settings.CUSTOM_URL if hasattr(settings, 'CUSTOM_URL') else '',
         'USE_EMAIL': settings.USE_EMAIL is True if hasattr(settings, 'USE_EMAIL') else False,
+
+        'DATA_SOURCES_EXPLORE_APPS': settings.DATA_SOURCES_EXPLORE_APPS if
+        hasattr(settings, 'DATA_SOURCES_EXPLORE_APPS') else [],
     }
