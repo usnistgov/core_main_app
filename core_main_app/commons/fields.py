9090d7236ea136f826febeb5099d399b4bb984cc
Philippe Dessauw <philippe.dessauw@nist.gov>
2017-06-09 15:43:06 -0400

    Added special field to check password policies
    Change-Id: I409c6b493edbf1426e5e2211ee93e556464323d3

---
+++ core_main_app/commons/fields.py
@@ -0,0 +1,20 @@
+"""
+
+"""
+from django import forms
+from core_main_app import settings
+from password_policies.forms.fields import PasswordPoliciesField
+from core_main_app.commons.validators import UpperCaseLetterCountValidator, LowerCaseLetterCountValidator
+
+
+class CustomPasswordPoliciesField(PasswordPoliciesField):
+    """
+A form field that validates a password using :ref:`api-validators`.
+"""
+    def __init__(self, *args, **kwargs):
+        if "widget" not in kwargs:
+            kwargs["widget"] = forms.PasswordInput(render_value=False)
+        kwargs["min_length"] = settings.PASSWORD_MIN_LENGTH
+        self.default_validators.append(UpperCaseLetterCountValidator(settings.PASSWORD_MIN_UPPERCASE_LETTERS))
+        self.default_validators.append(LowerCaseLetterCountValidator(settings.PASSWORD_MIN_LOWERCASE_LETTERS))
+        super(CustomPasswordPoliciesField, self).__init__(*args, **kwargs)
--- core_main_app/commons/validators.py
+++ core_main_app/commons/validators.py
@@ -1,9 +1,10 @@
 """ Common Validators """
-
+from django.utils.translation import ungettext
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 from django.utils.deconstruct import deconstructible
 from django.utils.encoding import force_text
+from password_policies.forms.validators import BaseCountValidator
 import os
 
 
@@ -19,7 +20,7 @@ class BlankSpacesValidator(object):
 
 @deconstructible
 class ExtensionValidator(object):
-    def __init__(self, valid_extensions=[]):
+    def __init__(self, valid_extensions=list()):
         self.valid_extensions = valid_extensions
 
     def __call__(self, value):
@@ -28,3 +29,79 @@ class ExtensionValidator(object):
             raise ValidationError(
                 _('Unsupported file extension.'),
             )
+
+
+class UpperCaseLetterCountValidator(BaseCountValidator):
+    """ Counts the occurrences of letters and raises a :class:`~django.core.exceptions.ValidationError` if the count
+    is less than :func:`~UpperCaseLetterCountValidator.get_min_count`.
+    """
+    categories = ['Lu']
+    """ The unicode data letter categories:
+
+    ====  ===========
+    Code  Description
+    ====  ===========
+    LC    Letter, Cased
+    Ll    Letter, Lowercase
+    Lu    Letter, Uppercase
+    Lt    Letter, Titlecase
+    Lo    Letter, Other
+    Nl    Number, Letter
+    ====  ===========
+    """
+    #: The validator's error code.
+    code = u"invalid_letter_count"
+
+    def __init__(self, min_uppercase_letters):
+        self.min_uppercase_letters = min_uppercase_letters
+
+    def get_error_message(self):
+        """ Returns this validator's error message.
+        """
+        msg = ungettext("The new password must contain %d or more uppercase letter.",
+                        "The new password must contain %d or more uppercase letters.",
+                        self.get_min_count()) % self.get_min_count()
+        return msg
+
+    def get_min_count(self):
+        """ Returns: Min uppercase letters
+        """
+        return self.min_uppercase_letters
+
+
+class LowerCaseLetterCountValidator(BaseCountValidator):
+    """ Counts the occurrences of letters and raises a :class:`~django.core.exceptions.ValidationError` if the count
+    is less than :func:`~LowerCaseLetterCountValidator.get_min_count`.
+    """
+    categories = ['Ll']
+    """
+    The unicode data letter categories:
+    ====  ===========
+    Code  Description
+    ====  ===========
+    LC    Letter, Cased
+    Ll    Letter, Lowercase
+    Lu    Letter, Uppercase
+    Lt    Letter, Titlecase
+    Lo    Letter, Other
+    Nl    Number, Letter
+    ====  ===========
+    """
+    #: The validator's error code.
+    code = u"invalid_letter_count"
+
+    def __init__(self, min_lowercase_letters):
+        self.min_lowercase_letters = min_lowercase_letters
+
+    def get_error_message(self):
+        """ Returns this validator's error message.
+        """
+        msg = ungettext("The new password must contain %d or more lower letter.",
+                        "The new password must contain %d or more lower letters.",
+                        self.get_min_count()) % self.get_min_count()
+        return msg
+
+    def get_min_count(self):
+        """ Returns: Min lowercase letters
+        """
+        return self.min_lowercase_letters
