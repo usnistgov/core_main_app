3e6afb8ad91e4968d6a0fe694ce652c620516223
Xavier SCHMITT <xavier.schmitt@nist.gov>
2017-06-15 09:42:47 -0400

    version 1.0.0-alpha2
    Change-Id: Ia7a8dd3ca96df070ae08eee3429863de98502382

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import find_packages, setup
 with open(os.path.join(os.path.dirname(__file__), "requirements.txt")) as f:
     required = f.read().splitlines()
 
-# allow setup.py to be run from any path
+# Allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="core_main_app",
-    version="1.0.0-alpha1",
+    version="1.0.0-alpha2",
     description=("core main Django package",),
     author="no_author",
     author_email="contact@example.com",
