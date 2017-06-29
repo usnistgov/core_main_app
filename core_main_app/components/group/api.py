6dc2ed800ef2f4dca990c555e43c8680e59d1be0
Xavier SCHMITT <xavier.schmitt@nist.gov>
2017-06-29 15:36:07 -0400

    add API to user and group for workspace
    Change-Id: I8d54c377867d78a0d254a52e674708e0772ce558

--- core_main_app/components/group/api.py
+++ core_main_app/components/group/api.py
@@ -4,6 +4,8 @@
 from django.contrib.auth.models import Group
 from django.db.models import Q
 
+import core_main_app.permissions.rights as rights
+
 
 def get_by_name_and_permission(name, permission_codename):
     """Get a group by name and permission codename.
@@ -16,3 +18,31 @@ def get_by_name_and_permission(name, permission_codename):
 
     """
     return Group.objects.filter(Q(name=name) & Q(permissions__codename=permission_codename))
+
+
+def get_group_by_id(group_id):
+    """ Return a group given its primary key.
+
+        Args:
+            group_id (str): Given group id
+
+        Returns:
+            Group object
+    """
+    return Group.objects.get(pk=group_id)
+
+
+def get_anonymous_group():
+    """ Get anonymous group of users.
+
+    Returns:
+    """
+    return Group.objects.filter(name=rights.anonymous_group).first()
+
+
+def get_default_group():
+    """ Get default group of users.
+
+    Returns:
+    """
+    return Group.objects.filter(name=rights.default_group).first()
