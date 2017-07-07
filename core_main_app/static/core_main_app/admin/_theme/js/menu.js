563c06c1d307412c7535f6d677e861a171f98606
Philippe Dessauw <philippe.dessauw@nist.gov>
2017-07-07 15:41:31 -0400

    Added item_count capability
    Change-Id: I4ff921ad605d297fa5d8acf2cc7476af9229209a

---
+++ core_main_app/static/core_main_app/admin/_theme/js/menu.js
@@ -0,0 +1,20 @@
+loadItemCount = function() {
+    $("#admin-menu").find(".item_count").each(function(index, element) {
+        var item_count_url = $(element).children("span").attr('id');
+
+        $.ajax({
+            url: item_count_url,
+            method: "GET",
+            success: function(data) {
+                if (data.count !== 0) {
+                    $(element).children("span").text(data.count);
+                    $(element).children("span").removeClass('hidden');
+                }
+            }
+        });
+    });
+};
+
+$(document).ready(function () {
+    loadItemCount();
+});
