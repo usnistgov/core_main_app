2f42261042c5bd3b8a8fea8be1131678062d68d8
Guillaume Sousa <guillaume.sousa@nist.gov>
2017-08-29 10:38:49 -0400

    Add common error page and show data access errors
    Change-Id: Ic4e5135e0fea5d1fcae2c127e4655542e39fc1f5

--- core_main_app/views/user/views.py
+++ core_main_app/views/user/views.py
@@ -11,8 +11,11 @@ from rest_framework.status import HTTP_405_METHOD_NOT_ALLOWED
 import core_main_app.components.data.api as data_api
 from core_main_app.components.version_manager import api as version_manager_api
 from core_main_app.settings import INSTALLED_APPS
+from core_main_app.utils.access_control.exceptions import AccessControlError
 from core_main_app.utils.rendering import render
 from core_main_app.views.user.forms import LoginForm
+
+from core_main_app.commons import exceptions
 
 
 def custom_login(request):
@@ -112,32 +115,41 @@ def data_detail(request):
 
     """
     data_id = request.GET['id']
+    error_message = ''
 
     try:
         data = data_api.get_by_id(data_id, request.user)
-    except:
-        # TODO: catch good exception, redirect to error page
-        pass
 
-    context = {
-        'data': data
-    }
+        context = {
+            'data': data
+        }
 
-    assets = {
-        "js": [
-            {
-                "path": 'core_main_app/common/js/XMLTree.js',
-                "is_raw": False
-            },
-            {
-                "path": 'core_main_app/user/js/data/detail.js',
-                "is_raw": False
-            },
-        ],
-        "css": ["core_main_app/common/css/XMLTree.css"],
-    }
+        assets = {
+            "js": [
+                {
+                    "path": 'core_main_app/common/js/XMLTree.js',
+                    "is_raw": False
+                },
+                {
+                    "path": 'core_main_app/user/js/data/detail.js',
+                    "is_raw": False
+                },
+            ],
+            "css": ["core_main_app/common/css/XMLTree.css"],
+        }
 
-    return render(request, 'core_main_app/user/data/detail.html', context=context, assets=assets)
+        return render(request, 'core_main_app/user/data/detail.html', context=context, assets=assets)
+    except AccessControlError:
+        error_message = 'Access forbidden'
+    except exceptions.DoesNotExist:
+        error_message = 'Data not found'
+    except exceptions.ModelError:
+        error_message = 'Model error'
+    except Exception, e:
+        error_message = 'Unexpected error'
+
+    return render(request, 'core_main_app/common/commons/error.html',
+                  context={"error": "Unable to access the requested data: {}.".format(error_message)})
 
 
 @login_required(login_url='/login')
