8f5fcaa9c2f8e62b3aa5c8b28549a38368361436
Xavier SCHMITT <xavier.schmitt@nist.gov>
2017-10-02 11:20:55 -0400

    update version to 1.0.0-alpha4
    Change-Id: I0d4c36b52670f7984528a8943140801566c3f3d5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="core_main_app",
-    version="1.0.0-alpha3",
+    version="1.0.0-alpha4",
     description=("core main Django package",),
     author="NIST IT Lab",
     author_email="itl_inquiries@nist.gov",
