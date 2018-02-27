3945141f55cf502826f5aea7328af801f36d56b3
Pierre Francois RIGODIAT <pierre-francois.rigodiat@nist.gov>
2018-02-27 08:16:31 -0500

    feat: Add DELETE/PATCH method - Give the ability to pass param for the tested view
    Change-Id: Iee759ca0ffed27a78a9b73b55408e4c5202b510d

--- core_main_app/utils/tests_tools/RequestMock.py
+++ core_main_app/utils/tests_tools/RequestMock.py
@@ -1,8 +1,9 @@
 """ Mock a Request object.
 """
-from django.test import RequestFactory
-from django.core.wsgi import get_wsgi_application
 import json
+
+from django.core.wsgi import get_wsgi_application
+from rest_framework.test import APIRequestFactory
 
 
 class RequestMock(object):
@@ -11,61 +12,95 @@ class RequestMock(object):
     """
 
     @staticmethod
-    def do_request_get(view, user, data=None):
+    def do_request_get(view, user, data=None, param=None):
         """Execute a GET HTTP request.
         Args:
             view: View method called by the request.
             user: User for the request.
             data: Data.
+            param: View method params.
 
         Returns:
             Response: Request response.
 
         """
-        return RequestMock._do_request("GET", view, user, data)
+        return RequestMock._do_request("GET", view, user, data, param)
 
     @staticmethod
-    def do_request_post(view, user, data=None):
+    def do_request_post(view, user, data=None, param=None):
         """Execute a POST HTTP request.
         Args:
             view: View method called by the request.
             user: User for the request.
             data: Data.
+            param: View method params.
 
         Returns:
             Response: Request response.
         """
-        return RequestMock._do_request("POST", view, user, data)
+        return RequestMock._do_request("POST", view, user, data, param)
 
     @staticmethod
-    def do_request_put(view, user, data=None):
+    def do_request_put(view, user, data=None, param=None):
         """Execute a PUT HTTP request.
         Args:
             view: View method called by the request.
             user: User for the request.
             data: Data.
+            param: View method params.
 
         Returns:
             Response: Request response.
 
         """
-        return RequestMock._do_request("PUT", view, user, data)
+        return RequestMock._do_request("PUT", view, user, data, param)
 
     @staticmethod
-    def _do_request(http_method, view, user, data=None):
+    def do_request_delete(view, user, data=None, param=None):
+        """Execute a DELETE HTTP request.
+        Args:
+            view: View method called by the request.
+            user: User for the request.
+            data: Data.
+            param: View method params.
+
+        Returns:
+            Response: Request response.
+
+        """
+        return RequestMock._do_request("DELETE", view, user, data, param)
+
+    @staticmethod
+    def do_request_patch(view, user, data=None, param=None):
+        """Execute a PATCH HTTP request.
+        Args:
+            view: View method called by the request.
+            user: User for the request.
+            data: Data.
+            param: View method params.
+
+        Returns:
+            Response: Request response.
+
+        """
+        return RequestMock._do_request("PATCH", view, user, data, param)
+
+    @staticmethod
+    def _do_request(http_method, view, user, data=None, param=None):
         """Execute the http_method request.
         Args:
             http_method: HTTP method.
             view: View method called by the request.
             user: User for the request.
             data: Data.
+            param: View method params.
 
         Returns:
             Response: Request response.
 
         """
         url = "/dummy_url"
-        factory = RequestFactory()
+        factory = APIRequestFactory()
         # Request by http_method.
         if http_method == "GET":
             request = factory.get(url, data=data)
@@ -73,6 +108,10 @@ class RequestMock(object):
             request = factory.post(url, data=data)
         elif http_method == "PUT":
             request = factory.put(url, data=json.dumps(data), content_type="application/json")
+        elif http_method == "DELETE":
+            request = factory.delete(url, data=json.dumps(data), content_type="application/json")
+        elif http_method == "PATCH":
+            request = factory.patch(url, data=json.dumps(data), content_type="application/json")
         # Set the user
         request.user = user
         # i18n. Get django validation messages.
@@ -80,4 +119,9 @@ class RequestMock(object):
         # Do not use CSRF checks.
         request._dont_enforce_csrf_checks = True
 
-        return view(request)
+        if param:
+            view_ = view(request, **param)
+        else:
+            view_ = view(request)
+
+        return view_
