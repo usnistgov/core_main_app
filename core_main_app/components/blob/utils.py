5d2464bad9fb591950d800548dcb3924605cfdd2
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2018-09-13 17:04:39 -0400

    fix: use SERVER_URI setting to build blob download uri
    Change-Id: Ic675265d8a5b857a2dea8dfce1c20a13ba17bbfb

--- core_main_app/components/blob/utils.py
+++ core_main_app/components/blob/utils.py
@@ -2,6 +2,8 @@
 """
 from django.core.urlresolvers import reverse
 
+from core_main_app.settings import SERVER_URI
+
 
 def get_blob_download_uri(blob, request):
     """ Return download url for the blob.
@@ -14,6 +16,6 @@ def get_blob_download_uri(blob, request):
 
     """
     # get URI to download blob
-    blob_download_uri = request.build_absolute_uri(reverse("core_main_app_rest_blob_download",
-                                                           kwargs={'pk': str(blob.id)}))
+    blob_download_uri = SERVER_URI + reverse("core_main_app_rest_blob_download", kwargs={'pk': str(blob.id)})
+
     return blob_download_uri
