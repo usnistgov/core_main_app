05939389fc2ca37041f0790c8bdba92cc1bb7af9
Adrien Catel <adrien.catel@nist.gov>
2018-10-22 13:11:46 -0400

    fix: Missing background in admin page to view data
    Change-Id: Ib65ae81ccd4a5dd2f16c189f5b764264efe9ee3a

--- core_main_app/admin.py
+++ core_main_app/admin.py
@@ -23,7 +23,7 @@ admin_urls = [
     url(r'^login', RedirectView.as_view(url=reverse_lazy("core_main_app_login"))),
     url(r'^logout', RedirectView.as_view(url=reverse_lazy("core_main_app_logout"))),
     url(r'^data$', common_views.ViewData.as_view(administration=True,
-                                                 template='core_main_app/admin/data/detail.html'),
+                                                 template='core_main_app/admin/data/view_data.html'),
         name='core_main_app_data_detail'),
     url(r'^templates$', admin_views.manage_templates, name='core_main_app_templates'),
     url(r'^template/upload$', admin_views.upload_template,
