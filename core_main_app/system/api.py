76a265d78d87d76f9cf15b2d513c26baf169616e
Philippe Dessauw <philippe.dessauw@nist.gov>
2018-11-07 14:46:44 -0500

    feat: move API from custom_queries, explore_tree
    Change-Id: I75b2db2f337d73be8a7b223af357d62326177f87

--- core_main_app/system/api.py
+++ core_main_app/system/api.py
@@ -1,4 +1,4 @@
-""" System api
+""" System API allowing to perform call on Data without access control. Use this API carefully.
 """
 from core_main_app.components.data.models import Data
 
@@ -12,3 +12,41 @@ def get_data_by_id(data_id):
         Returns: data object
     """
     return Data.get_by_id(data_id)
+
+
+def get_all_by_template(template):
+    """ Get all data belonging to the specified template.
+
+    Args:
+        template:
+
+    Returns:
+
+    """
+    return Data.objects(template=template)
+
+
+def get_all_by_list_template(list_template):
+    """ Get all data that belong to the template list.
+
+    Args:
+        list_template:
+
+    Returns:
+
+    """
+    return Data.get_all_by_list_template(list_template)
+
+
+def execute_query_with_projection(query, projection):
+    """ Execute a given query with a projection.
+
+    Args:
+        query:
+        projection:
+
+    Returns:
+
+    """
+    return Data.execute_query(query).only(projection)
+
