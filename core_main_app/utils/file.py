9a832d97291d1452902b0b4c9a277eba0a537a21
Adrien Catel <adrien.catel@nist.gov>
2018-12-11 12:24:27 -0500

    feat: add method to get filename from content disposition
    Change-Id: I47e2f540b20289f27b9714fdfd96f195fb3287a0

--- core_main_app/utils/file.py
+++ core_main_app/utils/file.py
@@ -1,14 +1,16 @@
-"""File utils
+""" File utils
 """
+import re
 from io import BytesIO
 from mimetypes import guess_type
 
-from core_main_app.commons.exceptions import CoreError
 from django.http.response import HttpResponse
+
+from core_main_app.commons.exceptions import CoreError
 
 
 def get_file_http_response(file_content, file_name, content_type=None, extension=''):
-    """Return http response with file to download.
+    """ Return http response with file to download
 
     Args:
         file_content:
@@ -44,7 +46,7 @@ def get_file_http_response(file_content, file_name, content_type=None, extension
 
 
 def read_file_content(file_path):
-    """Read the content of a file.
+    """ Read the content of a file
 
     Args:
         file_path:
@@ -55,3 +57,21 @@ def read_file_content(file_path):
     with open(file_path) as _file:
         file_content = _file.read()
         return file_content
+
+
+def get_filename_from_response(response):
+    """ Get filename from HTTP response
+
+    Args:
+        response: HTTP response
+
+    Returns:
+
+    """
+    content_disposition = response.headers.get('content-disposition')
+    if not content_disposition:
+        return None
+    file_name = re.findall('filename=(.+)', content_disposition)
+    if len(file_name) == 0:
+        return None
+    return file_name[0]
