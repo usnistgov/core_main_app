865d145c28ec0847235ac94ae08abcfad5021cc8
Adrien Catel <adrien.catel@nist.gov>
2018-12-12 09:14:36 -0500

    fix: flattener dependencies uses now requests for HTTPS compliance
    Change-Id: I8d7aed75f4c984632ac02a42c9f7e818d984cfd3

--- core_main_app/utils/xsd_flattener/xsd_flattener_database_url.py
+++ core_main_app/utils/xsd_flattener/xsd_flattener_database_url.py
@@ -1,19 +1,20 @@
 """ XSD Flattener Database or URL class
 """
-from core_main_app.utils.urls import get_template_download_pattern
-from xml_utils.xsd_flattener.xsd_flattener_url import XSDFlattenerURL
 from urlparse import urlparse
-from core_main_app.components.template import api as template_api
+
 from core_main_app.commons import exceptions
-from django.core.urlresolvers import reverse
+from core_main_app.components.template import api as template_api
+from core_main_app.utils.urls import get_template_download_pattern
+from core_main_app.utils.xsd_flattener.xsd_flattener_requests_url import XSDFlattenerRequestsURL
+from xml_utils.xsd_flattener.xsd_flattener_url import XSDFlattenerURL
 
 
-class XSDFlattenerDatabaseOrURL(XSDFlattenerURL):
+class XSDFlattenerDatabaseOrURL(XSDFlattenerRequestsURL):
     """ Get the content of the dependency from the database or from the URL.
     """
 
     def __init__(self, xml_string, download_enabled=True):
-        """Initializes the flattener
+        """ Initializes the flattener
 
         Args:
             xml_string:
---
+++ core_main_app/utils/xsd_flattener/xsd_flattener_requests_url.py
@@ -0,0 +1,33 @@
+""" XSD Flattener Requests URL Class
+"""
+from core_main_app.utils.requests_utils.requests_utils import send_get_request
+from xml_utils.xsd_flattener.xsd_flattener_url import XSDFlattenerURL
+
+
+class XSDFlattenerRequestsURL(XSDFlattenerURL):
+    """ Get the content of the dependency from the database or from the URL.
+    """
+
+    def __init__(self, xml_string, download_enabled=True):
+        """ Initializes the flattener
+
+        Args:
+            xml_string:
+            download_enabled:
+        """
+        XSDFlattenerURL.__init__(self, xml_string=xml_string, download_enabled=download_enabled)
+
+    def get_dependency_content(self, uri):
+        """ Get the content of the dependency from the URL using request util for HTTPS compliance
+
+        Args:
+            uri: Content URI
+
+        Returns:
+            Content
+
+        """
+        if self.download_enabled:
+            dependency_file = send_get_request(uri)
+            return dependency_file.content
+        return ""
