706325a069ab747f6f15439ae471908a6e8cfb26
Augustin Chini <augustin.chini@NIST.GOV>
2019-04-17 14:21:08 -0400

    feat: JS tools file and move escape predefined XML entities function in xml tools
    Change-Id: I26f79d0459ae1ba25f611fb9b332c03b0e218077

---
+++ core_main_app/static/core_main_app/common/js/debounce.js
@@ -0,0 +1,19 @@
+/**
+  * JS Debounce function
+  * @see https://davidwalsh.name/javascript-debounce-function
+  * @todo it's a reusable function should we put it in utils js file?
+  */
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
