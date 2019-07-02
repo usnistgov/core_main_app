b309d5622088d70b377d3e21c12a44563d1f2a62
Augustin CHINI <augustin.chini@nist.gov>
2019-07-02 16:02:25 -0400

    fix: logger improvement
    Change-Id: I56e361d9b3aa9a09c3a2df373de719dbb970882d

---
+++ core_main_app/templatetags/get_attribute.py
@@ -0,0 +1,17 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='get_attribute')
+def get_attribute(value, arg):
+    """@see https://stackoverflow.com/questions/844746/performing-a-getattr-style-lookup-in-a-django-template"""
+
+    if hasattr(value, str(arg)):
+        return getattr(value, arg)
+    elif isinstance(value, dict) and arg in value:
+        return value[arg]
+    elif arg.isdigit() and len(value) > int(arg):
+        return value[int(arg)]
+    else:
+        return ""
---
+++ tests/templatetags/test_get_attribute.py
@@ -0,0 +1,35 @@
+""" Unit tests of get_attribute templatetag
+"""
+from unittest.case import TestCase
+
+from core_main_app.templatetags.get_attribute import get_attribute
+
+
+class TestObject(object):
+    test = 'test'
+
+
+class TestSystemGetAllExcept(TestCase):
+
+    def test_get_attribute_on_empty_object(self):
+        result = get_attribute({}, 'test')
+        self.assertEqual(result, '')
+
+    def test_get_attribute_on_none(self):
+        result = get_attribute(None, 'test')
+        self.assertEqual(result, '')
+
+    def test_get_attribute_on_object(self):
+        test_obj = TestObject()
+        result = get_attribute(test_obj, 'test')
+        self.assertEqual(result, 'test')
+
+    def test_get_attribute_on_dict(self):
+        test_dict = {'test': 'test'}
+        result = get_attribute(test_dict, 'test')
+        self.assertEqual(result, 'test')
+
+    def test_get_attribute_on_table(self):
+        test_tab = ['test']
+        result = get_attribute(test_tab, '0')
+        self.assertEqual(result, 'test')
