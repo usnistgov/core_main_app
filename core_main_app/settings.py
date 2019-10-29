019102535edddf1491f4e89b2b0716dfe836fbff
Augustin CHINI <augustin.chini@nist.gov>
2019-10-29 09:21:05 -0400

    feat: local label customization
    Change-Id: I71457f341ca33d54bcc2fdf386c01ee5c136d7fc

--- core_main_app/settings.py
+++ core_main_app/settings.py
@@ -8,6 +8,10 @@ from django.conf import settings
 if not settings.configured:
     settings.configure()
 
+CUSTOM_NAME = getattr(settings, 'CUSTOM_NAME', 'Local')
+""" :py:class:`str`: Name of the local instance
+"""
+
 SECRET_KEY = getattr(settings, 'SECRET_KEY', '<secret_key>')
 """ :py:class:`str`: Django application secret key.
 """
--- core_main_app/utils/custom_context_processors.py
+++ core_main_app/utils/custom_context_processors.py
@@ -10,7 +10,7 @@ def domain_context_processor(request):
 
         'CUSTOM_TITLE': settings.CUSTOM_TITLE if hasattr(settings, 'CUSTOM_TITLE') else '',
         'CUSTOM_ORGANIZATION': settings.CUSTOM_ORGANIZATION if hasattr(settings, 'CUSTOM_ORGANIZATION') else '',
-        'CUSTOM_NAME': settings.CUSTOM_NAME if hasattr(settings, 'CUSTOM_NAME') else '',
+        'CUSTOM_NAME': settings.CUSTOM_NAME,
         'CUSTOM_SUBTITLE': settings.CUSTOM_SUBTITLE if hasattr(settings, 'CUSTOM_SUBTITLE') else '',
         'CUSTOM_DATA': settings.CUSTOM_DATA if hasattr(settings, 'CUSTOM_DATA') else '',
         'CUSTOM_URL': settings.CUSTOM_URL if hasattr(settings, 'CUSTOM_URL') else '',
