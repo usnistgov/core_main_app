05a4a9de6efd3088fbd8f9e0f3b15297e520e22b
Philippe Dessauw <philippe.dessauw@nist.gov>
2019-12-18 11:50:17 -0500

    fix: add markdown support for login msg + cleanup
    Change-Id: Icd988b0daf2c230c0363fd5ae4abf89d8032b821

---
+++ core_main_app/utils/markdown_parser.py
@@ -0,0 +1,22 @@
+""" Parser to convert Markdown to HTML in a safe way
+"""
+from markdown import markdown
+
+from xml_utils.commons.exceptions import HTMLError
+from xml_utils.html_tree.parser import safe_html
+
+
+def parse(text):
+    """ Parse Markdown to convert it into HTML
+
+    Args:
+        text:
+
+    Returns:
+    """
+    md_text = markdown(text)
+
+    try:
+        return safe_html(md_text)
+    except HTMLError as e:
+        return str(e)
--- core_main_app/views/user/views.py
+++ core_main_app/views/user/views.py
@@ -7,19 +7,18 @@ from django.contrib.auth.forms import (
     PasswordResetForm, SetPasswordForm)
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.staticfiles import finders
-from django.urls import reverse
 from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect, resolve_url
+from django.urls import reverse
 from django.urls import reverse_lazy
-from django.utils.http import urlsafe_base64_decode
-from django.views.decorators.cache import never_cache
-from django.views.decorators.debug import sensitive_post_parameters
-from rest_framework.status import HTTP_405_METHOD_NOT_ALLOWED
 from django.utils.encoding import force_text
+from django.utils.http import urlsafe_base64_decode
+from rest_framework.status import HTTP_405_METHOD_NOT_ALLOWED
 
 import core_main_app.components.web_page_login.api as web_page_login_api
 from core_main_app.components.version_manager import api as version_manager_api
 from core_main_app.settings import INSTALLED_APPS
+from core_main_app.utils.markdown_parser import parse
 from core_main_app.utils.rendering import render
 from core_main_app.views.user.forms import LoginForm
 
@@ -78,17 +77,29 @@ def default_custom_login(request):
             user = authenticate(username=username, password=password)
 
             if not user.is_active:
-                return render(request, "core_main_app/user/login.html",
-                              context={'login_form': LoginForm(initial={'next_page': next_page}), 'login_locked': True,
-                                       'with_website_features': "core_website_app" in INSTALLED_APPS})
+                return render(
+                    request, "core_main_app/user/login/main.html", context={
+                        "login_form": LoginForm(
+                            initial={"next_page": next_page}
+                        ),
+                        "login_locked": True,
+                        "with_website_features":
+                            "core_website_app" in INSTALLED_APPS
+                    }
+                )
 
             login(request, user)
 
             return _login_redirect(next_page)
-        except Exception as e:
-            return render(request, "core_main_app/user/login.html",
-                          context={'login_form': LoginForm(initial={'next_page': next_page}), 'login_error': True,
-                                   'with_website_features': "core_website_app" in INSTALLED_APPS})
+        except Exception:
+            return render(
+                request, "core_main_app/user/login/main.html", context={
+                    "login_form": LoginForm(initial={"next_page": next_page}),
+                    "login_error": True,
+                    "with_website_features":
+                        "core_website_app" in INSTALLED_APPS
+                }
+            )
     elif request.method == "GET":
         if request.user.is_authenticated:
             return redirect(reverse("core_main_app_homepage"))
@@ -98,10 +109,12 @@ def default_custom_login(request):
             next_page = request.GET["next"]
 
         # build the context
-        context = {'login_form': LoginForm(initial={"next_page": next_page}),
-                   'with_website_features': "core_website_app" in INSTALLED_APPS}
+        context = {
+            "login_form": LoginForm(initial={"next_page": next_page}),
+            "with_website_features": "core_website_app" in INSTALLED_APPS
+        }
         assets = {
-            "css": ["core_main_app/user/css/login/login.css"]
+            "css": ["core_main_app/user/css/login.css"]
         }
 
         # get the web page login if exist
@@ -109,29 +122,27 @@ def default_custom_login(request):
 
         if web_page_login:
             # update the context
-            context["login_message"] = web_page_login.content
+            context["login_message"] = parse(web_page_login.content)
 
             # if exist we build assets and modals collection
             assets.update({
-                "js": [
-                    {
-                        "path": 'core_main_app/user/js/web_page_login/web_page_login.js',
-                        "is_raw": False
-                    }
-                ]
+                "js": [{
+                    "path": "core_main_app/user/js/login/message.js",
+                    "is_raw": False
+                }]
             })
-            modals = ["core_main_app/user/web_page_login/modals/web_page_login_modal.html"]
+            modals = ["core_main_app/user/login/modals/login_message.html"]
 
             # render the page with context, assets and modals
             return render(request,
-                          "core_main_app/user/login.html",
+                          "core_main_app/user/login/main.html",
                           context=context,
                           assets=assets,
                           modals=modals)
 
         # render the page with context, assets
         return render(request,
-                      "core_main_app/user/login.html",
+                      "core_main_app/user/login/main.html",
                       context=context,
                       assets=assets)
     else:
@@ -158,20 +169,16 @@ def homepage(request):
 
         Returns:
     """
-    assets = {
-        "js": []
-    }
+    assets = dict()
 
     if finders.find("core_main_app/css/homepage.css") is not None:
         assets["css"] = ["core_main_app/css/homepage.css"]
 
     if finders.find("core_main_app/js/homepage.js") is not None:
-        assets["js"].append(
-            {
-                "path": "core_main_app/js/homepage.js",
-                "is_raw": False
-            }
-        )
+        assets["js"] = {
+            "path": "core_main_app/js/homepage.js",
+            "is_raw": False
+        }
 
     return render(request, "core_main_app/user/homepage.html", assets=assets)
 
@@ -192,24 +199,24 @@ def manage_template_versions(request, version_manager_id):
         version_manager = version_manager_api.get(version_manager_id)
 
         if not request.user.is_staff and version_manager.user != str(request.user.id):
-            raise Exception("You don't have the rights to perform this action.")
+            raise Exception("You do not have the rights to perform this action.")
 
         context = get_context_manage_template_versions(version_manager)
-        if 'core_parser_app' in settings.INSTALLED_APPS:
+        if "core_parser_app" in settings.INSTALLED_APPS:
             context.update({"module_url": "core_parser_app_template_modules"})
 
         assets = {
             "js": [
                 {
-                    "path": 'core_main_app/common/js/templates/versions/set_current.js',
+                    "path": "core_main_app/common/js/templates/versions/set_current.js",
                     "is_raw": False
                 },
                 {
-                    "path": 'core_main_app/common/js/templates/versions/restore.js',
+                    "path": "core_main_app/common/js/templates/versions/restore.js",
                     "is_raw": False
                 },
                 {
-                    "path": 'core_main_app/common/js/templates/versions/modals/disable.js',
+                    "path": "core_main_app/common/js/templates/versions/modals/disable.js",
                     "is_raw": False
                 }
             ]
@@ -218,14 +225,14 @@ def manage_template_versions(request, version_manager_id):
         modals = ["core_main_app/admin/templates/versions/modals/disable.html"]
 
         return render(request,
-                      'core_main_app/common/templates/versions.html',
+                      "core_main_app/common/templates/versions.html",
                       assets=assets,
                       modals=modals,
                       context=context)
     except Exception as e:
         return render(request,
-                      'core_main_app/common/commons/error.html',
-                      context={'error': str(e)})
+                      "core_main_app/common/commons/error.html",
+                      context={"error": str(e)})
 
 
 def get_context_manage_template_versions(version_manager, object_name="Template"):
@@ -266,9 +273,9 @@ def get_context_manage_template_versions(version_manager, object_name="Template"
 
 
 def custom_reset_password(request,
-                          template_name='core_main_app/user/registration/password_reset_form.html',
-                          email_template_name='core_main_app/user/registration/password_reset_email.html',
-                          subject_template_name='core_main_app/user/registration/password_reset_subject.txt',
+                          template_name="core_main_app/user/registration/password_reset_form.html",
+                          email_template_name="core_main_app/user/registration/password_reset_email.html",
+                          subject_template_name="core_main_app/user/registration/password_reset_subject.txt",
                           password_reset_form=PasswordResetForm,
                           token_generator=default_token_generator,
                           post_reset_redirect=None,
@@ -296,29 +303,29 @@ def custom_reset_password(request,
     """
 
     if post_reset_redirect is None:
-        post_reset_redirect = reverse('password_reset_done')
+        post_reset_redirect = reverse("password_reset_done")
     else:
         post_reset_redirect = resolve_url(post_reset_redirect)
     if request.method == "POST":
         form = password_reset_form(request.POST)
         if form.is_valid():
             opts = {
-                'use_https': request.is_secure(),
-                'token_generator': token_generator,
-                'from_email': from_email,
-                'email_template_name': email_template_name,
-                'subject_template_name': subject_template_name,
-                'request': request,
-                'html_email_template_name': html_email_template_name,
-                'extra_email_context': extra_email_context,
+                "use_https": request.is_secure(),
+                "token_generator": token_generator,
+                "from_email": from_email,
+                "email_template_name": email_template_name,
+                "subject_template_name": subject_template_name,
+                "request": request,
+                "html_email_template_name": html_email_template_name,
+                "extra_email_context": extra_email_context,
             }
             form.save(**opts)
             return HttpResponseRedirect(post_reset_redirect)
     else:
         form = password_reset_form()
     context = {
-        'form': form,
-        'title': 'Password reset',
+        "form": form,
+        "title": "Password reset",
     }
     if extra_context is not None:
         context.update(extra_context)
@@ -327,8 +334,8 @@ def custom_reset_password(request,
 
 
 def custom_password_reset_done(request,
-                        template_name='core_main_app/user/registration/password_reset_done.html',
-                        extra_context=None):
+                               template_name="core_main_app/user/registration/password_reset_done.html",
+                               extra_context=None):
     """ Custom password reset done page.
 
         Parameters:
@@ -341,7 +348,7 @@ def custom_password_reset_done(request,
     """
 
     context = {
-        'title': 'Password reset sent',
+        "title": "Password reset sent",
     }
 
     assets = {
@@ -353,12 +360,12 @@ def custom_password_reset_done(request,
 
     return render(request, template_name, assets=assets, context=context)
 
+
 def custom_password_reset_confirm(request, uidb64=None, token=None,
-                           template_name='core_main_app/user/registration/password_reset_confirm.html',
-                           token_generator=default_token_generator,
-                           set_password_form=SetPasswordForm,
-                           post_reset_redirect=None,
-                           extra_context=None):
+                                  template_name="core_main_app/user/registration/password_reset_confirm.html",
+                                  token_generator=default_token_generator,
+                                  set_password_form=SetPasswordForm,
+                                  post_reset_redirect=None, extra_context=None):
     """
     View that checks the hash in a password reset link and presents a
     form for entering a new password.
@@ -377,24 +384,24 @@ def custom_password_reset_confirm(request, uidb64=None, token=None,
         :return request
     """
 
-    UserModel = get_user_model()
+    user_model = get_user_model()
 
     assert uidb64 is not None and token is not None  # checked by URLconf
     if post_reset_redirect is None:
-        post_reset_redirect = reverse('password_reset_complete')
+        post_reset_redirect = reverse("password_reset_complete")
     else:
         post_reset_redirect = resolve_url(post_reset_redirect)
     try:
         # urlsafe_base64_decode() decodes to bytestring on Python 3
         uid = force_text(urlsafe_base64_decode(uidb64))
-        user = UserModel._default_manager.get(pk=uid)
-    except (TypeError, ValueError, OverflowError, UserModel.DoesNotExist):
+        user = user_model._default_manager.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, user_model.DoesNotExist):
         user = None
 
     if user is not None and token_generator.check_token(user, token):
         validlink = True
-        title = 'Enter new password'
-        if request.method == 'POST':
+        title = "Enter new password"
+        if request.method == "POST":
             form = set_password_form(user, request.POST)
             if form.is_valid():
                 form.save()
@@ -404,11 +411,11 @@ def custom_password_reset_confirm(request, uidb64=None, token=None,
     else:
         validlink = False
         form = None
-        title = 'Password reset unsuccessful'
+        title = "Password reset unsuccessful"
     context = {
-        'form': form,
-        'title': title,
-        'validlink': validlink,
+        "form": form,
+        "title": title,
+        "validlink": validlink,
     }
     if extra_context is not None:
         context.update(extra_context)
@@ -417,8 +424,8 @@ def custom_password_reset_confirm(request, uidb64=None, token=None,
 
 
 def custom_password_reset_complete(request,
-                            template_name='core_main_app/user/registration/password_reset_complete.html',
-                            extra_context=None):
+                                   template_name="core_main_app/user/registration/password_reset_complete.html",
+                                   extra_context=None):
     """
     Custom password reset complete page.
 
@@ -431,8 +438,8 @@ def custom_password_reset_complete(request,
         :return request
     """
     context = {
-        'login_url': resolve_url(settings.LOGIN_URL),
-        'title': 'Password reset complete',
+        "login_url": resolve_url(settings.LOGIN_URL),
+        "title": "Password reset complete",
     }
 
     assets = {
