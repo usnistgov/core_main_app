e8395480c11f2f6ad8b1891dd12679f0b45b7492
Augustin CHINI <augustin.chini@nist.gov>
2020-01-24 16:05:25 -0500

    feat: display the last modification date
    Change-Id: I83fd64709f08f6a908b331117a2ae68e07c5e3b9

---
+++ core_main_app/templatetags/json_date.py
@@ -0,0 +1,36 @@
+""" Templatetags to get JSON date format and display according to parsing directives
+"""
+import logging
+
+import dateutil.parser
+
+from django import template
+from django.template.defaultfilters import stringfilter
+from django.utils import formats
+
+logger = logging.getLogger(__name__)
+register = template.Library()
+
+
+@register.filter(name="json_date")
+@stringfilter
+def json_date(value, directive):
+    """ get JSON date string input and return a formatted date string according to the parsing directive
+
+    Args:
+        value: input JSON date string
+        directive: standard django date format directive
+
+    See: https://docs.djangoproject.com/fr/3.0/ref/templates/builtins/#date
+    Returns: formatted string
+    """
+    formatted_date = value
+    try:
+        # parse the JSON date
+        input_datetime = dateutil.parser.parse(formatted_date)
+        # format the date thanks to the directive for UI display
+        formatted_date = formats.date_format(input_datetime, directive)
+        return formatted_date
+    except Exception as e:
+        logger.error(str(e))
+        return formatted_date
