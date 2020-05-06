c29cc32ea980301cc0968d8b1e805d1feb47ec39
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2020-05-06 13:38:16 -0400

    fix: add missing is_public from workspace serializer
    Change-Id: I841e1ee04c3b02749007239309ae2c4887b5de95

--- core_main_app/rest/workspace/serializers.py
+++ core_main_app/rest/workspace/serializers.py
@@ -15,11 +15,15 @@ class WorkspaceSerializer(DocumentSerializer):
         model = Workspace
         fields = ["id",
                   "title",
-                  "owner"]
+                  "owner",
+                  "is_public"]
         read_only_fields = ('id', 'owner')
 
     def create(self, validated_data):
         """
         Create and return a new `workspace` instance, given the validated data.
         """
-        return workspace_api.create_and_save(validated_data['title'], validated_data['user'].id)
+        return workspace_api.create_and_save(title=validated_data['title'],
+                                             owner_id=validated_data['user'].id,
+                                             is_public=validated_data['is_public']
+                                             if 'is_public' in validated_data else False)
