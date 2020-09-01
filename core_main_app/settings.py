99470231058525cb29898091346865829e3a49b5
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2020-09-01 08:44:10 -0400

    docs: add settings doc
    Change-Id: I90f911cdb6fb7619ab9d232cdec85349c13f90e6

--- core_main_app/settings.py
+++ core_main_app/settings.py
@@ -1,4 +1,7 @@
-""" Settings for core_main_app. These settings are overwritten at project level.
+""" Settings for core_main_app.
+
+Settings with the following syntax can be overwritten at the project level:
+SETTING_NAME = getattr(settings, "SETTING_NAME", "Default Value")
 """
 import os
 from os.path import join
