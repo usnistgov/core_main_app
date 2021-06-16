7cffdd5ce6f0d835f8665883afa320e0bf4d2f3c
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2021-06-16 11:08:51 -0400

    feat: load saml2 settings from env
    Change-Id: Id80842f6433f49b9dff3031e041def0629b558b7

--- core_main_app/utils/saml2/utils.py
+++ core_main_app/utils/saml2/utils.py
@@ -2,6 +2,117 @@
 """
 import os
 
+import saml2
+import saml2.saml
+
+
+def load_attribute_map_from_env():
+    """Load attribute map from environment (https://pysaml2.readthedocs.io/en/latest/howto/config.html#attribute-map-dir)
+
+    Returns:
+
+    """
+    return {
+        "identifier": "urn:oasis:names:tc:SAML:2.0:attrname-format:basic",
+        "to": {
+            "uid": os.environ["SAML_ATTRIBUTES_MAP_UID"],
+            "mail": os.environ["SAML_ATTRIBUTES_MAP_EMAIL"],
+            "sn": os.environ["SAML_ATTRIBUTES_MAP_SN"],
+            "cn": os.environ["SAML_ATTRIBUTES_MAP_CN"],
+        },
+    }
+
+
+def load_saml_config_from_env(server_uri, base_dir):
+    """Load SAML configuration from environment (https://pysaml2.readthedocs.io/en/latest/howto/config.html#configuration-of-pysaml2-entities)
+
+    Args:
+        server_uri:
+        base_dir:
+
+    Returns:
+
+    """
+
+    contact_person = load_contact_person_from_env()
+    organization = load_organization_from_env()
+    return {
+        # full path to the xmlsec1 binary programm
+        "xmlsec_binary": os.getenv("SAML_XMLSEC_BIN_PATH", "/usr/bin/xmlsec1"),
+        # your entity id, usually your subdomain plus the url to the metadata view
+        "entityid": f"{server_uri}/saml2/metadata/",
+        # directory with attribute mapping
+        "attribute_map_dir": os.path.join(
+            base_dir, os.getenv("SAML_ATTRIBUTE_MAP_DIR", "attr-maps")
+        ),
+        # this block states what services we provide
+        "service": {
+            # we are just a lonely SP
+            "sp": {
+                "want_response_signed": os.getenv(
+                    "SAML_WANT_RESPONSE_SIGNED", "False"
+                ).lower()
+                == "true",
+                "want_assertions_signed": os.getenv(
+                    "SAML_WANT_ASSERTIONS_SIGNED", "False"
+                ).lower()
+                == "true",
+                "name": os.getenv("SERVER_NAME", "Curator"),
+                "name_id_format": saml2.saml.NAMEID_FORMAT_TRANSIENT,
+                # For Okta add signed logout requets. Enable this:
+                "logout_requests_signed": True,
+                "endpoints": {
+                    # url and binding to the assertion consumer service view
+                    # do not change the binding or service name
+                    "assertion_consumer_service": [
+                        (f"{server_uri}/saml2/acs/", saml2.BINDING_HTTP_POST),
+                    ],
+                    # url and binding to the single logout service view
+                    # do not change the binding or service name
+                    "single_logout_service": [
+                        # Disable next two lines for HTTP_REDIRECT for IDP's that only support HTTP_POST. Ex. Okta:
+                        (f"{server_uri}/saml2/ls/", saml2.BINDING_HTTP_REDIRECT),
+                        (f"{server_uri}/saml2/ls/post/", saml2.BINDING_HTTP_POST),
+                    ],
+                },
+                # Mandates that the identity provider MUST authenticate the
+                # presenter directly rather than rely on a previous security context.
+                "force_authn": False,
+                # Enable AllowCreate in NameIDPolicy.
+                "name_id_format_allow_create": False,
+                # attributes that this project need to identify a user
+                "required_attributes": ["givenName", "sn", "mail"],
+            },
+        },
+        # where the remote metadata is stored, local, remote or mdq server.
+        # One metadata store or many ...
+        "metadata": {
+            "remote": [
+                {"url": os.getenv("SAML_METADATA_REMOTE")},
+            ],
+        },
+        # Signing
+        "key_file": os.path.join(base_dir, os.environ["SAML_KEY_FILE"])
+        if "SAML_KEY_FILE" in os.environ
+        else None,  # private part
+        "cert_file": os.path.join(base_dir, os.environ["SAML_CERT_FILE"])
+        if "SAML_CERT_FILE" in os.environ
+        else None,  # public part
+        # Encryption
+        "encryption_keypairs": [
+            {
+                "key_file": os.path.join(base_dir, os.environ["SAML_KEY_FILE"])
+                if "SAML_KEY_FILE" in os.environ
+                else None,  # private part
+                "cert_file": os.path.join(base_dir, os.environ["SAML_CERT_FILE"])
+                if "SAML_CERT_FILE" in os.environ
+                else None,  # public part
+            }
+        ],
+        "contact_person": contact_person,
+        "organization": organization,
+    }
+
 
 def load_contact_person_from_env():
     """Load PySAML2 contact person configurations from environment (https://pysaml2.readthedocs.io/en/latest/howto/config.html#contact-person)
