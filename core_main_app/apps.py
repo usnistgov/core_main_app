ae23dd0f254cccd5d3ee5ef97a93c876f29ffca1
Guillaume Sousa <guillaume.sousa@nist.gov>
2022-01-09 22:08:35 -0500

    feat: upgrade celery and use django-celery-beat for periodic tasks
    Change-Id: Ibf1a1ebb86d9884cace7c40167b2bf1717c9a45f

--- core_main_app/apps.py
+++ core_main_app/apps.py
@@ -3,8 +3,10 @@
 import sys
 
 from django.apps import AppConfig
+from django.conf import settings
 
 import core_main_app.permissions.discover as discover
+from core_main_app.commons.exceptions import CoreError
 from core_main_app.components.data.models import Data
 from core_main_app.settings import SSL_CERTIFICATES_DIR
 from core_main_app.utils.databases.mongoengine_database import init_text_index
@@ -21,6 +23,14 @@ class InitApp(AppConfig):
     def ready(self):
         """When the app is ready, run the discovery and init the indexes."""
         if "migrate" not in sys.argv:
+            # check celery settings
+            if (
+                settings.CELERYBEAT_SCHEDULER
+                != "django_celery_beat.schedulers:DatabaseScheduler"
+            ):
+                raise CoreError(
+                    "CELERYBEAT_SCHEDULER setting needs to be set to 'django_celery_beat.schedulers:DatabaseScheduler'."
+                )
             check_ssl_certificates_dir_setting(SSL_CERTIFICATES_DIR)
             discover.init_rules(self.apps)
             discover.create_public_workspace()
