59adb236f3180f2348d75a9fdedfeae085bde204
hamza <hnb8@nsit.gov>
2022-06-30 11:38:25 -0400

    feat: add loading spinner
    Change-Id: Ica50c15a0cbe4907ccbf1aca9c4133fa3099ae4b
    (cherry picked from commit 2545e24a5fa9cd03cd2150fa0380d70459ec42af)

---
+++ core_main_app/static/core_main_app/common/js/loading_spinner.js
@@ -0,0 +1,28 @@
+
+/**
+ * Show spinner for button
+ */
+var showSpinner = function($btnID) {
+     // Show loading spinner
+     $btnID.attr('class','fas fa-spinner fa-spin');
+}
+
+/**
+ *  Hide spinner for button
+ */
+var hideSpinner = function($btnID,icon) {
+    // show old btn icon
+    $btnID.attr('class',icon)
+}
+
+
+/**
+ * Show spinner for container
+ */
+var displaySpinner = function(container) {
+     // clear container
+     container.html("");
+     // display loading spinner
+     container.append($("<i class='fa fa-spinner fa-spin fa-3x fa-fw loading-spinner'></i>"));
+}
+
