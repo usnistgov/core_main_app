0c3c269d9cfe7481abd2fca9280f4c73d16902c0
hamza <hnb8@nsit.gov>
2023-02-09 14:56:36 -0500

    fix: hide xsd editor url
    Change-Id: Ic610d2965ee93a01d7d37baf3e23fdd558f9f3fe

--- core_main_app/urls.py
+++ core_main_app/urls.py
@@ -45,14 +45,6 @@ urlpatterns = [
         name="core_main_app_xml_text_editor_view",
     ),
     re_path(
-        r"^xsd-editor/template",
-        login_required(
-            common_views.XSDEditor.as_view(),
-            login_url=reverse_lazy("core_main_app_login"),
-        ),
-        name="core_main_app_xsd_text_editor_view",
-    ),
-    re_path(
         r"^template/versions/(?P<version_manager_id>\w+)",
         user_views.manage_template_versions,
         name="core_main_app_manage_template_versions",
