e60552f8fb3906781d037ef860557f2af445db36
Guillaume Sousa <guillaume.sousa@nist.gov>
2023-02-23 22:26:36 +0000

    chore: upgrade 2.3.0
    Change-Id: I0cf3234f7774175166767aa553176a5ad302e017

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_main_app",
-    version="2.2.0",
+    version="2.3.0",
     description="Main functionalities for the curator core project",
     long_description=long_desc,
     author="NIST IT Lab",
