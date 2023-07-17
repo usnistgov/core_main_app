e3ca823b16c606b5bb5a08c6ec416124c95c8d1a
Philippe Dessauw <philippe.dessauw@nist.gov>
2023-07-17 11:11:27 -0400

    fix: notification popup scrolling out of frame on big pages
    Change-Id: Iad4ee4c67c74a0ec50fefdf8daa57f96d63527a4

--- core_main_app/static/core_main_app/common/js/elementViewport.js
+++ core_main_app/static/core_main_app/common/js/elementViewport.js
@@ -4,7 +4,6 @@
   * @todo it's a reusable function should we put it in utils js file?
   */
 function isElementInViewport (el){
-
     //special bonus for those using jQuery
     if (typeof jQuery === "function" && el instanceof jQuery) {
         el = el[0];
