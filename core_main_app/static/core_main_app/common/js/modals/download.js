c3f7050e6dd6891ea377af9cf132b70339fd3546
pdessauw <philippe.dessauw@nist.gov>
2026-02-24 17:20:57 -0500

    fix: space in translation causing actions to silently fail
    Change-Id: Ia9a8556373bbcae81b015d14fb6618136a824f80

--- core_main_app/static/core_main_app/common/js/modals/download.js
+++ core_main_app/static/core_main_app/common/js/modals/download.js
@@ -1,11 +1,10 @@
 /**
  * AJAX call, download document
- * @param document
  */
 download = function(e){
     let toFormat = $('#format').is(':checked')
     let downloadUrl;
-    if (e.data.document == "document") downloadUrl = downloadDocumentUrl
+    if (e.data.document === "document") downloadUrl = downloadDocumentUrl
     else downloadUrl = downloadTemplateUrl
     window.location.href = downloadUrl +"?pretty_print=" + toFormat;
 
